ea856e595b11abfb37b9caf8abdb81e684117625
Andrey Efimov <efiand@ya.ru>
2022-08-06 14:06:57 +0300

    fix contenteditable checker

--- source/scripts/_modules/_editable.js
+++ source/scripts/_modules/_editable.js
@@ -1,9 +1,15 @@
-document.querySelectorAll('h1, h2, h3, p, b, a, button, label, legend').forEach((element) => {
+const CONTENT_TAGS = ['h1, h2, h3, p, b, a, button, label, legend, blockquote, cite'];
+const CONTROL_TAGS = ['input', 'textarea'];
+
+const isElementControl = (tagName) => document.activeElement.tagName.toLowerCase() === tagName;
+const isControlElementActive = () => CONTROL_TAGS.some(isElementControl);
+
+document.querySelectorAll(...CONTENT_TAGS).forEach((element) => {
 	element.spellcheck = false;
 });
 
 document.addEventListener('keydown', (evt) => {
-	if (evt.key.toLowerCase() === 'e') {
+	if (evt.key.toLowerCase() === 'e' && !isControlElementActive()) {
 		document.body.contentEditable = document.body.contentEditable !== 'true';
 	}
 });
